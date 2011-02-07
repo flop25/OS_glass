2a294ce4dd8f7e2fdb586abf31ade46dda810509
flop25 <flop25@piwigo.org>
2011-02-07 18:35:17 +0000

    first commit : update for 2.1 and higher the features in picture.tpl are in reverse : the floating box disappeared
    git-svn-id: http://piwigo.org/svn/extensions/floOS/OS_glass@9109 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ themeconf.inc.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Theme Name: OS_glass
+Version: 1.0.0
+Description: 
+Theme URI: http://fr.piwigo.org/ext/extension_view.php?eid=383
+Author: flop25
+Author URI: http://www.planete-flop.fr/site.php?acc=file
+*/
+
+$themeconf = array(
+  'name'         => 'OS_glass',
+  'parent'        => 'OS_default',
+  'icon_dir'      => 'themes/OS_default/icon',
+  'mime_icon_dir' => 'themes/OS_default/icon/mimetypes/',
+  'local_head' => 'local_head.tpl',
+  'activable' => true,
+);
+
+?>
